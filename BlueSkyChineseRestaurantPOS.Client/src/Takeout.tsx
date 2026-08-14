6679ad2f97ca640524c0cf5a53138820aeb5a721
ttan815 <tonytan815@gmail.com>
2026-08-14 01:14:07 -0700

    Added react logic for configuring data types used for loading list of items.

--- BlueSkyChineseRestaurantPOS.Client/src/Takeout.tsx
+++ BlueSkyChineseRestaurantPOS.Client/src/Takeout.tsx
@@ -8,21 +8,53 @@ import './App.css'
 function Takeout() {
     const location = useLocation()
     const navigate = useNavigate()
+    const [itemCount, setItemCount] = useState(1)
+    const [discountPercent, setDiscountPercent] = useState(0)
+    const [discountConstant, setDiscountConstant] = useState(0)
+    const [subtotal, setSubtotal] = useState(0)
+    const [tax, setTax] = useState(.0875)
+    const [total, setTotal] = useState(0)
+    const [orderedItems, setOrderedItems] = useState([[]])
+    const [itemCustomization, setItemCustomization] = useState([])
     const phoneNumber = location.state?.phoneNumber;
+    function addFoodToOrder(foodName: string, foodPrice: number) {
+        const foodItem = {
+            itemName: foodName,
+            chineseItemname: "?",
+            price: foodPrice,
+            orderedAmount: itemCount,
+            menuItemCustomization: itemCustomization
+
+        }
+    }
+    const order = {
+        orderTime: new Date().toISOString(),
+        orderList: orderedItems,
+        discountPercent: discountPercent,
+        discountConstant: discountConstant,
+        subtotal: 0,
+        tax: tax,
+        total: total
+    }
     if (!phoneNumber) {
         return (
             <>
                 <h1>No phone number provided</h1>
-                <button onClick={()=>navigate("/") }>Back to Main Menu</button>
+                <button onClick={() => navigate("/")}>Back to Main Menu</button>
             </>
         )
     }
     return (
         <>
-
-            if(!phoneNumber
-            <form>
-            </form>
+            <div className="item-list-div">
+                {orderedItems.map(item =>
+                    <div>{item}</div>
+                )}
+            </div>
+            <div>
+                <button onClick={()=>addFoodToOrder("Orange Chicken", 15)} >Orange Chicken</button>
+            </div>
+            <h1>{phoneNumber}</h1>
         </>
     )
 }
