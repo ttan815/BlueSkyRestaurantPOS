2c50ccba487bbf00ac86955a333df151689e2c6e
ttan815 <tonytan815@gmail.com>
2026-08-15 00:29:56 -0700

    Added a small of options, added highlighting of selected item.

--- BlueSkyChineseRestaurantPOS.Client/src/Takeout.tsx
+++ BlueSkyChineseRestaurantPOS.Client/src/Takeout.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 import reactLogo from './assets/react.svg'
@@ -9,16 +9,37 @@ function Takeout() {
     const location = useLocation()
     const navigate = useNavigate()
     const [itemCount, setItemCount] = useState(1)
+    const [itemID, setItemID] = useState(0) // Temporary until IDs appear from DB
     const [discountPercent, setDiscountPercent] = useState(0)
     const [discountConstant, setDiscountConstant] = useState(0)
     const [subtotal, setSubtotal] = useState(0)
     const [tax, setTax] = useState(.0875)
     const [total, setTotal] = useState(0)
-    const [orderedItems, setOrderedItems] = useState([[]])
+    interface itemCustomizationConstruct {
+        modification: string,
+        modificationName: string,
+        modificationChineseName: string,
+        priceChange: number,
+        description: string
+    }
+    interface foodItemConstruct {
+        id: number
+        itemName: string,
+        chineseItemname: string,
+        price: number,
+        orderedAmount: number,
+        menuItemCustomization: itemCustomizationConstruct[]
+    }
+
+    const [orderedItems, setOrderedItems] = useState<foodItemConstruct[]>([]);
     const [itemCustomization, setItemCustomization] = useState([])
     const phoneNumber = location.state?.phoneNumber;
+    const [selectedItemID, setSelectedItemID] = useState(-999);
     function addFoodToOrder(foodName: string, foodPrice: number) {
+        var newID = itemID + 1;
+        setItemID(newID);
         const foodItem = {
+            id: itemID,
             itemName: foodName,
             chineseItemname: "?",
             price: foodPrice,
@@ -26,6 +47,12 @@ function Takeout() {
             menuItemCustomization: itemCustomization
 
         }
+        setOrderedItems([...orderedItems, foodItem]);
+        newID = itemID + 1;
+        setItemID(newID);
+    }
+    function setSelectedItem(id: number) {
+        setSelectedItemID(id);
     }
     const order = {
         orderTime: new Date().toISOString(),
@@ -46,13 +73,54 @@ function Takeout() {
     }
     return (
         <>
+            <div className="order-options">
+                <button>
+                    Modify
+                </button>
+                <button>
+                    To Go
+                </button>
+                <button>
+                    Split
+                </button>
+                <button>
+                    Print
+                </button>
+                <button>
+                    Payment
+                </button>
+            </div>
             <div className="item-list-div">
                 {orderedItems.map(item =>
-                    <div>{item}</div>
+                    <div>
+                        {selectedItemID === item.id ? 
+                        
+                            <button onClick={() => setSelectedItem(item.id)} className="selected-food-item-button">
+                                <div key={item.id} >{item.itemName}</div>
+                            </button>
+                            :
+                            <button key={item.id} onClick={() => setSelectedItem(item.id)} className="default-food-item-button">
+                                <div>{item.itemName}</div>
+                            </button>
+                        }
+                        {
+                            item.menuItemCustomization.map(customization =>
+                                <div>
+                                    <button>
+                                        <p>{customization.modification}</p>
+                                        <p>{customization.modificationName}</p>
+                                        <p>{customization.modificationChineseName}</p>
+                                        <p>{customization.priceChange}</p>
+                                        <p>{customization.description}</p>
+                                    </button>
+                                </div>)
+
+                        }
+                </div>
                 )}
             </div>
             <div>
-                <button onClick={()=>addFoodToOrder("Orange Chicken", 15)} >Orange Chicken</button>
+                <button key={918273} onClick={()=>addFoodToOrder("Orange Chicken", 15)} >Orange Chicken</button>
             </div>
             <h1>{phoneNumber}</h1>
         </>
